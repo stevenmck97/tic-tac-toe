3e3e4059263da6886fda5ea35f056d5962f1cd6c
Steven McKeown <stevenmckeown19@gmail.com>
2022-03-21 17:16:18 +0000

    Created createPlayer factory function and 2 module functions (gameboard and gameController). Created 3 public functions in gameBoard which create an array with a placeholder marker value of X, displays the board and adds a data attribute corresponding to their index in the array, creates an array from the children of the container element and iterates through them and when clicked sets the text content to a placeholder of O then updates the array element value

--- script.js
+++ script.js
@@ -1,8 +1,59 @@
-(function addMarker() {
-    const gridItem = document.querySelectorAll(".grid-item");
-    gridItem.forEach((item) => {
-        item.addEventListener("click", () => {
-            item.textContent = "x";
+// (function addMarker() {
+//     const gridItem = document.querySelectorAll(".grid-item");
+//     gridItem.forEach((item) => {
+//         item.addEventListener("click", () => {
+//             item.textContent = "x";
+//         });
+//     });
+// })();
+
+// Two players (x and o)
+// 9 div gameboard
+// player has to be able to select x or o as their marker
+// an array needs to be created to store their choices and grid positions?
+// create logic that 3 in a row of one marker in specific grid positions will win the game or round
+// create logic to stop players from choosing a spot already taken
+
+const createPlayer = (name, marker) => {
+    return { name, marker };
+};
+
+const gameBoard = (() => {
+    const boardContainer = document.querySelector(".container");
+    let board = [];
+    let marker = "X";
+
+    const createArray = () => {
+        for (let i = 0; i < 9; i++) {
+            board.push(marker);
+        }
+        // console.log(board);
+    };
+
+    const createBoard = () => {
+        board.forEach((item, index) => {
+            const boardItem = document.createElement("div");
+            boardItem.className = `board-item`;
+            boardItem.dataset.number = index;
+            boardContainer.appendChild(boardItem);
         });
-    });
+    };
+
+    const addMarker = () => {
+        Array.from(boardContainer.children).forEach((item, index) => {
+            item.addEventListener("click", () => {
+                item.textContent = "O";
+                board[index] = "O";
+                console.log(board);
+            });
+        });
+    };
+
+    return { board, createArray, createBoard, addMarker };
+})();
+
+const gameController = (() => {
+    gameBoard.createArray();
+    gameBoard.createBoard();
+    gameBoard.addMarker();
 })();
